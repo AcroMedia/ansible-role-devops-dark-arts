fdaba2e5e994068c888c1a24d06c7acf08cd8996
Chithra K <63741367+Chithrak07@users.noreply.github.com>
2021-09-21 04:44:22 +0530

    Add support for php 8.0 (#18)
    * Add support for php 8.0
    Co-authored-by: Dale Anderson <danderson@acromedia.com>

--- scripts/deployables/acro-add-website.sh
+++ scripts/deployables/acro-add-website.sh
@@ -497,6 +497,8 @@ function sanity_checks_pass () {
      : # OK
   elif  [[ "${PHP_VERSION}" == "7.4" ]]; then
     : # OK
+  elif  [[ "${PHP_VERSION}" == "8.0" ]]; then
+    : # OK
   else
     cerr "${BOLD}ERR:${UNBOLD} This script isn't built to handle a PHP_VERSION value of: $PHP_VERSION"
     cerr "The PHP_VERSION variable is used to name some files, and to control services."
